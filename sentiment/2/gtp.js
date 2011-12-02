c1903b89b830adc42ea283ca5279006f9218aa11
Michael Lawrence <byteface@googlemail.com>
2011-12-02 10:23:44 +0000

    just made it an object for the sentiment stuff

--- sentiment/2/gtp.js
+++ sentiment/2/gtp.js
@@ -1,71 +1,60 @@
-<!DOCTYPE HTML>
-<html lang="en">
-
-	<head>
-		<title>proccess GPU</title>		
-		<meta charset="utf-8">
-	</head>
-
-	<body>
-	   
-<canvas id="can" width="452", height="452"></canvas>
-
-<script type="text/javascript">
-
-var im = new Image();
-im.onload = imageLoaded;
-im.src = "ImageGenTest.png";
+Gtp = {
 
 // mappings for unwrapping
-var map =['','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0',',','.','\'','\"','-','[',']','?','!',';',':','(',')','*','<','>','/','\\','@','£','$','%','&','-','+','_','|','{','}',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '];
+map :['','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0',',','.','\'','\"','-','[',']','?','!',';',':','(',')','*','<','>','/','\\','@','£','$','%','&','-','+','_','|','{','}',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '],
 
 
-function imageLoaded(ev) {
+    init : function( file )
+    {
+        var im = new Image();
+        
+        var selfRef = this;
+        
+        im.onload = function( e ){ selfRef.imageLoaded( e ) };
+        im.src = file;//"ImageGenTest.png";
+    },
 
-	element = document.getElementById("can");
-	c = element.getContext("2d");
 
-	im = ev.target; // the image, assumed to be 783x783 current negative map
-	
-	// read the width and height of the canvas
-	width = parseInt(element.getAttribute("width"));
-	height = parseInt(element.getAttribute("height"));
-
-	// stamp the image on the left of the canvas:
-	c.drawImage(im, 0, 0);
-
-	// get all canvas pixel data
-	imageData = c.getImageData(0, 0, width, height);
-    var pix = imageData.data;
-
-    var str = "";
+    imageLoaded : function ( ev ) {
     
-    var i=0;
-    var len=pix.length;
+        element = document.getElementById("can");
+        c = element.getContext("2d");
     
-    var arr=[];
+        im = ev.target; // the image, assumed to be 783x783 current negative map
+        
+        // read the width and height of the canvas
+        width = parseInt(element.getAttribute("width"));
+        height = parseInt(element.getAttribute("height"));
     
-    for( i; i<len; i+=4 ) {
-        arr.push( map[pix[i]] );
-        arr.push( map[pix[i+1]] );
-        arr.push( map[pix[i+2]] );
-//        str += map[pix[i+3]]+"";                        
+        // stamp the image on the left of the canvas:
+        c.drawImage(im, 0, 0);
+    
+        // get all canvas pixel data
+        imageData = c.getImageData(0, 0, width, height);
+        var pix = imageData.data;
+    
+        var str = "";
+        
+        var i=0;
+        var len=pix.length;
+        
+        var arr=[];
+        
+        for( i; i<len; i+=4 ) {
+            arr.push( this.map[pix[i]] );
+            arr.push( this.map[pix[i+1]] );
+            arr.push( this.map[pix[i+2]] );
+    //        str += map[pix[i+3]]+"";                        
+        }
+            
+        //arr.join("");
+            
+        document.write( arr.join("") );
+      
+      
+    //    window.console.log(str);
+      
     }
-        
-    //arr.join("");
-        
-    document.write( arr.join("") );
-  
-  
-//    window.console.log(str);
-  
+
+
 }
-
-
-</script>
-
-
-
-				
-	</body>
-</html>
