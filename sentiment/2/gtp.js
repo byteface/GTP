365dec0a4281c5ff3c0ea2d636728fa8687e8b15
Michael Lawrence <byteface@googlemail.com>
2011-12-02 11:33:04 +0000

    generating query images in canvas to run against the maps. created todo for creating tokenised mash up maps that will utilise r for data, g and b for tokenisation

--- sentiment/2/gtp.js
+++ sentiment/2/gtp.js
@@ -54,7 +54,205 @@ map :['','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r'
       
     //    window.console.log(str);
       
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
+
+        characters you want to encode / remember you will need to unwrap these so your unwrap array must derive from this
+        
+        */
+        encodeMap : {   'a' : 1,
+                        'b' : 2,
+                        'c' : 3,
+                        'd' : 4,
+                        'e' : 5,
+                        'f' : 6,
+                        'g' : 7,
+                        'h' : 8,
+                        'i' : 9,
+                        'j' : 10,
+                        'k' : 11,
+                        'l' : 12,
+                        'm' : 13,
+                        'n' : 14,
+                        'o' : 15,
+                        'p' : 16,
+                        'q' : 17,
+                        'r' : 18,
+                        's' : 19,
+                        't' : 20,
+                        'u' : 21,
+                        'v' : 22,
+                        'w' : 23,
+                        'x' : 24,
+                        'y' : 25,
+                        'z' : 26,
+                        'A' : 27,
+                        'B' : 28,
+                        'C' : 29,
+                        'D' : 30,
+                        'E' : 31,
+                        'F' : 32,
+                        'G' : 33,
+                        'H' : 34,
+                        'I' : 35,
+                        'J' : 36,
+                        'K' : 37,
+                        'L' : 38,
+                        'M' : 39,
+                        'N' : 40,
+                        'O' : 41,
+                        'P' : 42,
+                        'Q' : 43,
+                        'R' : 44,
+                        'S' : 45,
+                        'T' : 46,
+                        'U' : 47,
+                        'V' : 48,
+                        'W' : 49,
+                        'X' : 50,
+                        'Y' : 51,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+                        'Z' : 52,
+                        '1' : 53,
+                        '2' : 54,
+                        '3' : 55,
+                        '4' : 56,
+                        '5' : 57,
+                        '6' : 58,
+                        '7' : 59,
+                        '8' : 60,
+                        '9' : 61,
+                        '0' : 62,
+                        ',' : 63,
+                        '.' : 64,                                                                                                                                                                                                                                                                                                                                                                                                
+                        '\'' : 65,
+                        '\"' : 66,
+                        '-' : 67,
+                        '[' : 68,
+                        ']' : 69,
+                        '?' : 70,
+                        '!' : 71,
+                        ';' : 72,
+                        ':' : 73,
+                        '(' : 74,
+                        ')' : 75,
+                        '*' : 76,
+                        '<' : 77,
+                        '>' : 78,
+                        '/' : 79,
+                        '\\' : 80,
+                        '@' : 81,
+                        '£' : 82,
+                        '$' : 83,
+                        '%' : 84,
+                        '&' : 85,
+                        '-' : 86,
+                        '+' : 87,
+                        '_' : 88,
+                        '|' : 89,
+                        '{' : 90,
+                        '}' : 91,
+                        ' ' : 255                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+                         },
+// TODO - note im using 244 for failure to recognise char in colorise method
+
+
+
+    createDataImage : function ( data )
+    {
+
+        var contents = data;
+        var contentsLength = contents.length;
+        var squared = Math.ceil( Math.sqrt( contentsLength/3 ) ); // round up the square root and divide by 3 as were using 3 channels
+        
+       // window.console.log( squared ); 
+        // TODO - need to get lines too so can add extra pixels as line seperators... implement fget - note same for this now in js version
+        
+        
+                // - TODO - dynamic width / height based on square size needed
+//        jQuery('<canvas id="new" width="400", height="400"></canvas>').html("").appendTo('#myDiv');
+        document.write('<canvas id="new" width="' + squared + '", height="' + squared + '"></canvas>' );
+        
+        
+        element = document.getElementById("new");
+        c = element.getContext("2d");
+    
+        // read the width and height of the canvas
+        width = parseInt(element.getAttribute("width"));
+        height = parseInt(element.getAttribute("height"));
+                
+        imageData = c.createImageData(width, height);     
+
+        var xPos = 0;
+        var yPos = 0;            
+        
+        for( var charCount = 0; charCount < contentsLength; charCount+=4 )
+        {
+            var char1 = contents.substr( charCount, 1);
+            var char2 = contents.substr( charCount+1, 1);
+            var char3 = contents.substr( charCount+2, 1);
+            
+//            var selfRef = this;
+            var col = this.setPixel( imageData, xPos, yPos, this.colorise( char1 ), this.colorise( char2 ), this.colorise( char3 ), 0xff );
+            
+            xPos++;
+            
+            if(xPos>squared){
+                xPos=0;
+                yPos+=1;
+            }
+        }
+
+        c.putImageData( imageData, 0, 0 );
+      
+    },
+
+
+    colorise: function ( character )
+    {
+        var col = 244; // if a character is unrecognised it will become space for now
+
+        if( this.encodeMap[character] ){
+            col = this.encodeMap[character];            
+        }else{
+            // TODO - some of these aren't showing... question mark seems to throw although i have it
+            // error_log( $character );
+        }
+        
+        return col;
+    },
+
+
+    setPixel : function ( imageData, x, y, r, g, b, a ) // TODO - add and use the alpha channel
+    {
+        var index = (x + y * imageData.width) * 4;
+        imageData.data[index+0] = r;
+        imageData.data[index+1] = g;
+        imageData.data[index+2] = b;
+        imageData.data[index+3] = a;
     }
+
+
+
 
 
 }
