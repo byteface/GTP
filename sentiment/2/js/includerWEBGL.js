dc421598fd7bc60d71b9b1003f8a8a9d26ed4fd8
Michael Lawrence <byteface@googlemail.com>
2011-12-14 21:25:56 +0000

    testing texture stuff from lessons, appear to be getting it on the stage now that i realise power 2 rule for images

--- sentiment/2/js/includerWEBGL.js
+++ sentiment/2/js/includerWEBGL.js
@@ -8,3 +8,6 @@ js_includer("js/app/SentimentCanvas.js");
 js_includer("js/app/SentimentWEBGL.js");
 js_includer("js/app/Loader.js");
 js_includer("js/app/Map.js");
+
+js_includer("js/glMatrix-0.9.5.min.js"); // TODO - think i saw a better matrix operator recently. may replace
+js_includer("js/webgl-utils.js");
